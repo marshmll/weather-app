246acad7171c5a77c1bde782af84c1f7d102dab4
marshmll <renandasilvaoliveiraandrade@gmail.com>
2021-08-19 15:56:38 -0300

    Add favicon

--- pages/index.js
+++ pages/index.js
@@ -180,10 +180,9 @@ export default function Home() {
   return (
     <>
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
-        ></link>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"></link>
+        <link rel="icon" href="https://www.svgrepo.com/show/119535/sun.svg" type="image/svg" />
+        <title>Weather App</title>
       </Head>
       <Styles />
       <header className="header">
